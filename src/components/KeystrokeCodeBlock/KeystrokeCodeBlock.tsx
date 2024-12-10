7c634d048a2bff1a5e2234d3dd2c30510f5a43b6
David Goss <david@davidgoss.co>
2024-12-10 20:18:09 +0000

    noop hook structure

--- src/components/KeystrokeCodeBlock/KeystrokeCodeBlock.tsx
+++ src/components/KeystrokeCodeBlock/KeystrokeCodeBlock.tsx
@@ -1,10 +1,12 @@
 import React, {FC} from "react";
 import CodeBlock from "@theme/CodeBlock";
+import {useContentByKeystroke} from "./useContentByKeystroke";
 
 export const KeystrokeCodeBlock: FC<{content: string}> = ({content}) => {
+    const partialContent = useContentByKeystroke(content)
     return <CodeBlock
         language="gherkin"
         showLineNumbers>
-        {content}
+        {partialContent}
     </CodeBlock>
 }
---
+++ src/components/KeystrokeCodeBlock/useContentByKeystroke.ts
@@ -0,0 +1,3 @@
+export function useContentByKeystroke(content: string) {
+    return content
+}
