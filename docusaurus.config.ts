5fba470a898a8717589370a99f9db53362acfb24
davidjgoss <david@davidgoss.co>
2024-12-18 16:26:16 +0000

    Allow robots to index (#50)

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -100,7 +100,6 @@ export default {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     {
-      metadata: [{ name: 'robots', content: 'noindex' }],
       colorMode: {
         defaultMode: 'light',
       },
